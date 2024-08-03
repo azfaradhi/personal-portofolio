4946649192284c2e5c07e81ff99bed425c8173ee
azfaradhi <aradhihakim@gmail.com>
2024-08-03 11:07:35 +0700

    feat: add inisial file

---
+++ personal-portofolio/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center'>
+    <p> PERSONAL PORTOFOLIO </p>
+    </div>
+  )
+}
+
+export default page
