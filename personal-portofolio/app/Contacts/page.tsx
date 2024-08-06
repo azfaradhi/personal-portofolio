42b9102d1acb6ddcf0ab27909044dbed50bb9b2d
azfaradhi <aradhihakim@gmail.com>
2024-08-07 06:11:55 +0700

    feat: adding contacts page

---
+++ personal-portofolio/app/Contacts/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navigation from '@/components/Navigation'
+
+
+const Contacts = () => {
+  return (
+    <div className='bg-gradient-to-r from-[#291C2E] to-[#3B1A22] min-h-screen'>
+      <div className='flex flex-col items-center'>
+        <Navigation />
+        <div className='container flex flex-col mt-20 justify-start'>
+          <h1 className='text-white text-4xl font-medium'>Contact</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contacts
